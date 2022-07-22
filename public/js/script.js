71506b5d4d36d9e34be7473beafda0865f851c66
Hitesh <hites_297@yahoo.co.in>
2022-07-22 00:06:51 -0400

    style start page

--- public/js/script.js
+++ public/js/script.js
@@ -11,6 +11,8 @@ window.onload = () => {
         var x = document.getElementById("demo");
         x.innerHTML = "Latitude: " + position.coords.latitude +
             "<br>Longitude: " + position.coords.longitude;
+        document.getElementById('status').style.display = 'block';
+        document.getElementById('message').style.display = 'none';
         window.location.href = `/wallpapers?lat=${position.coords.latitude}&long=${position.coords.longitude}&page=1`;
     }
 }
